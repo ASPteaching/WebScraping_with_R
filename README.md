2ad3fc1989a8369c4c9e576bdba70214171b4501
alexsanchezpla <asanchez@ub.edu>
2019-03-26 06:56:36 +0100

    Checked packages needed and minor updates

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This site contains the materials for 
 
 - A course on getting data from the web using R that I have taught several times with minor changes
-- A short talk on web scraping to be given at the "Young STatisticians Meeting" in the IBC2018 Conference in Barcelona.
+- A talk on web scraping that I gave at the "Young Statisticians Meeting" in the *International Biometrics Conference 2018* in Barcelona.
 - A longer version of the previous talk
 - Some materials and links,
 
--- WebScraping_course/R_packages_4_the_course.R
+++ WebScraping_course/R_packages_4_the_course.R
@@ -1,23 +1,51 @@
 # Rlibraries to be used in the Web Scraping course
-
-# 1 - Intro & Parsing html
-
 if(!require(rvest)) install.packages("rvest")
-if(!require(xml2)) install.packages("xml2")
-if(!require(magrittr)) install.packages("magrittr")
-
-# 2 - Regular expressions
-if(!require(stringi)) install.packages("stringi")
-if(!require(stringr)) install.packages("stringr")
-
-# 3 - Twitter
 if(!require(dplyr)) install.packages("dplyr")
 if (!require(twitteR)) install.packages("twitteR")
 if (!require(ROAuth)) install.packages("ROAuth")
-if (!require(httr)) install.packages("httr")
 if (!require(tm)) install.packages("tm")
 if (! require(wordcloud) ) install.packages("wordcloud")
+if (! require(XML) ) install.packages("XML")
+
+# 1 - Intro & Parsing html
+
+# if(!require(rvest)) install.packages("rvest")
+# Other packages required that are automatically installed: 
+# ‘sys’# 
+# ‘askpass’# 
+# ‘glue’# 
+# ‘stringi’# 
+# ‘Rcpp’# 
+# ‘curl’# 
+# ‘jsonlite’# 
+# ‘mime’# 
+# ‘openssl’# 
+# ‘R6’# 
+# ‘stringr’# 
+# ‘xml2’# 
+# ‘httr’# 
+# ‘selectr’# 
+# ‘magrittr’
+
+# 2 - General packages
+# if(!require(dplyr)) install.packages("dplyr")
+
+# 3 - Twitter
+# if (!require(twitteR)) install.packages("twitteR")
+# ‘bit’# 
+# ‘bit64’# 
+# ‘rjson’# 
+# ‘DBI’# 
+
+# if (!require(ROAuth)) install.packages("ROAuth")
+# ‘bitops’
+# ‘RCurl’
+# ‘digest’
+
+# if (!require(tm)) install.packages("tm")
+# if (! require(wordcloud) ) install.packages("wordcloud")
+# ‘NLP’
+# ‘slam’
 
 # 4 - XML
-if (! require(XML) ) install.packages("XML")
-if (! require(xml2) ) install.packages("xml2")
+# if (! require(XML) ) install.packages("XML")
