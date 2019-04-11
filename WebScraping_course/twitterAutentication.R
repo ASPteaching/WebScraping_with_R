da94db175e7fa1e19a94f3e32e28defc8b9e3f23
Alex Sánchez Pla <asanchez@ub.edu>
2019-04-11 08:05:04 +0200

    Reviewed and updated twitter and XML slides

--- WebScraping_course/twitterAutentication.R
+++ WebScraping_course/twitterAutentication.R
@@ -1,6 +1,6 @@
 # Use this code chunk to assign values to these 4 variables.
 # The values can be obtained from **your** twitter developer account.
-key <- "XMAYl2tKiRYro43E9rSQG870Z"   # your Consumer Key (API Key)
-secret <-   "a446H2hii2WZeTwXLQqnDpIL0dnRbauoCkgO4L7jsljt4cLEPR"  # Consumer Secret (API Secret)
-mytoken <-  "240357022-oBmZoFcJy7OpqLG1UNRDXavYtSxggcO2bnbIFQ77"    # "Access Token from Twitter"
-secretoken <- "duwWUjsaK4jIEePuIN6d9iOO9MJl06wj6M9QaO6MOjxfa"   # "Access Token Secret from Twitter"
+key <- "VSV2Gf8mlEfkotIkNjioafFme"   # your Consumer Key (API Key)
+secret <-   "v4cxJ8eLYJsCW64mChmIoGX2Y0Pzd193nFHm1fMPL68tyR12k5"  # Consumer Secret (API Secret)
+mytoken <-  "240357022-cwqM5AhFs0kBNktW3jI0wtX4pHWTg01HXaQ3Xhdx"    # "Access Token from Twitter"
+secretoken <- "0xjCfxdFLxIACkJKhuzRx2qSUJeXmCR4iuEwTQNbqRblp"   # "Access Token Secret from Twitter"
