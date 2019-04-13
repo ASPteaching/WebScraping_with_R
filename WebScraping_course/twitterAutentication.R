ee236b194f9e41a25cf12cb61cc0373ec6b09285
Alex Sánchez Pla <asanchez@ub.edu>
2019-04-13 13:59:18 +0200

    as .gitignore is not ignoring what I want to be ignored I have removed the files :-(

--- WebScraping_course/twitterAutentication.R
+++
@@ -1,6 +0,0 @@
-# Use this code chunk to assign values to these 4 variables.
-# The values can be obtained from **your** twitter developer account.
-key <- "VSV2Gf8mlEfkotIkNjioafFme"   # your Consumer Key (API Key)
-secret <-   "v4cxJ8eLYJsCW64mChmIoGX2Y0Pzd193nFHm1fMPL68tyR12k5"  # Consumer Secret (API Secret)
-mytoken <-  "240357022-cwqM5AhFs0kBNktW3jI0wtX4pHWTg01HXaQ3Xhdx"    # "Access Token from Twitter"
-secretoken <- "0xjCfxdFLxIACkJKhuzRx2qSUJeXmCR4iuEwTQNbqRblp"   # "Access Token Secret from Twitter"
